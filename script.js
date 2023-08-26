85f31dc2bcdd2d1230508e2744a5cfa209a211fb
object-ions <moshikoatia@gmail.com>
2023-08-26 14:26:57 -0600

    validate input and append input to DOM

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 // Get DOM elements
 const itemForm = document.querySelector('#item-form');
 const itemInput = document.querySelector('#item-input');
+itemInput.focus();
 const itemList = document.querySelector('#item-list');
 
 // Add item to list
 function addItem(e) {
     e.preventDefault();
     
+    const itemInputValue = itemInput.value;
+
     // Validate input
-    if (itemInput.value === '') {
+    if (itemInputValue === '') {
         popupMessage('Please enter an item');
     } else {
-        false;
+        
+        // Create li item
+        const li = document.createElement('li');
+        li.innerText = itemInputValue;
+
+        // Create X button
+        const xButton = document.createElement('button');
+        xButton.className = 'remove-item btn-link text-red';
+
+        // Create Icon
+        const icon = document.createElement('i');
+        icon.className = 'fa-solid fa-xmark';
+
+        // Append icon to xButton
+        xButton.appendChild(icon);
+
+        // Append xButton to li
+        li.appendChild(xButton);
+
+        // Append li to itemList
+        itemList.appendChild(li);
+
+        // Clear input and focus
+        itemInput.value = '';
+        itemInput.focus();
+        
     }
+
 }
 
 function popupMessage(message) {
-    console.log(message);
     const popup = document.querySelector('#popup');
     popup.innerHTML = message;
     popup.classList.add('show');
